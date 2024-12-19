321ebb8cd5bba9d172128a06197fed08a04ac9a7
frankie-q22 <frankiequintero3@gmail.com>
2024-12-18 23:27:09 -0700

    added snake

---
+++ # Snake Game.py
@@ -0,0 +1,11 @@
+
+import pygame
+
+pygame.init()
+
+#Creates Game Window
+Screen_Width =  1000
+Screen_Height = 600
+
+Screen = pygame.display.set_mode((Screen_Width,Screen_Height))
+pygame.display.set_caption("Snake ~~~~:P")
