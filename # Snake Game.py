767e76a9f7959805e0ec6622c15cf638fb40ba43
frankie-q22 <frankiequintero3@gmail.com>
2024-12-25 18:30:05 -0700

    commit

--- # Snake Game.py
+++ # Snake Game.py
@@ -8,16 +8,21 @@ pygame.init()
 Screen_Width =  800
 Screen_Height = 700
 Tile_Size = 50
-Range = (Tile_Size // 2 , Screen_Width - Tile_Size // 2 , Tile_Size)
-
+Range_X = (Tile_Size, Screen_Width - Tile_Size, Tile_Size)
+Range_Y = (Tile_Size, Screen_Height - Tile_Size, Tile_Size)
 # tuple for randomzing both x and y coordinates
-Randomize_Position = lambda: [randrange(*Range), randrange(*Range)]
+Randomize_Position = lambda: [randrange(*Range_X), randrange(*Range_Y)]
 
 snake = pygame.rect.Rect([0, 0, Tile_Size -2, Tile_Size -2])
-snake.center = Randomize_Position()
 length = 1
 segments = [snake.copy()]
 snake_direction = (0,0)
+apple = pygame.rect.Rect([0, 0, Tile_Size -2, Tile_Size -2])
+apple.center = Randomize_Position()
+#add score
+#score = 0
+#score + 1
+time, time_step = 0, 110
 Screen = pygame.display.set_mode((Screen_Width,Screen_Height))
 pygame.display.set_caption("Snake ~~~~:P   ")
 
@@ -37,11 +42,26 @@ while True:
          if event.key == pygame.K_d:
              snake_direction =(Tile_Size,0)
  Screen.fill('Black')
+ #borders and eating yourself
+ #cant_eat_yourself = pygame.Rect.collidelist(snake, segments[:-1]) != -1
+ if snake.left < 0 or snake.right > 800 or snake.top < 0 or snake.bottom > 700: #or #cant_eat_yourself:
+     snake.center, apple.center = Randomize_Position(), Randomize_Position()
+     length, snake_direction = 1, (0,0)
+     segments = [snake.copy()]
+ #check to see if apple was eaten
+ if snake.center == apple.center:
+     apple.center = Randomize_Position()
+     length += 1
+ #draw apple
+ pygame.draw.rect(Screen, 'red', apple)
  #function draws snake, and also makes appear in a different position everytime
  [pygame.draw.rect(Screen, 'green', segment) for segment in segments]
  #Allows snake to move
- snake.move_ip(snake_direction)
- segments.append(snake.copy())
- segments = segments[- length:]
+ current_timing = pygame.time.get_ticks()
+ if current_timing - time > time_step:
+     time = current_timing
+     snake.move_ip(snake_direction)
+     segments.append(snake.copy())
+     segments = segments[- length:]
  pygame.display.flip()
  clock.tick(60)   
